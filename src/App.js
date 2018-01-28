abcb8e3749233bd1c80a50f29703c6ca1a1c8d49
mrtan <freemrtan@gmail.com>
2018-01-28 21:46:28 +0800

    完成第一章内容，添加模块热更新

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ Component } from "react";
+import './App.css'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  render(){
+    const me = {name: 'tanxiaolong', sex:'male'}
+    return(
+      <div className='App'>
+        <h2>my name is {me.name}, a {me.sex} coder.</h2>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
