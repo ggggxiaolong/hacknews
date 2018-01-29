33192b8d8b2cf8f2ab9ab3ebfe7c92c43838cbb9
mrtan <freemrtan@gmail.com>
2018-01-29 22:07:48 +0800

    update to page 1000

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import './App.css'
+
+const list = [
+  {
+    title: 'React',
+    url:'https://facebook.github.io',
+    author:'Jordan Walke',
+    num_comments:3,
+    point:4,
+    objectIs:0,
+  },
+  {
+    title:'Redux',
+    url:'https://github.com/reactjs/redux',
+    author:'Dan Abramov, Adrew Clark',
+    num_comments:2,
+    point:5,
+    objectIs:1,
+  }
+]
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      list: list,
+      name: "xiao ming"
+    }
+  }
   render(){
-    const me = {name: 'tanxiaolong', sex:'male'}
-    return(
-      <div className='App'>
-        <h2>my name is {me.name}, a {me.sex} coder.</h2>
+    return (
+      <div className="App">
+        <h2>{this.state.name}</h2>
+        {
+          this.state.list.map( item =>
+              <div key={item.objectIs}>
+                <span> <a href={item.url}>{item.title}</a></span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.point}</span>
+              </div>
+          )}
       </div>
     )
   }
 }
+
 export default App
---
+++ src/es6.js
@@ -0,0 +1,13 @@
+const name = "xiao ming"
+const user = {name,}
+
+var userService = {
+    getUserName(){
+        return "haah";
+    }
+}
+
+const key ='name'
+const user2 = {
+     [key]:"xiao ming"
+ }
