6223aa47902f902805bee7bf49b207250e2232fb
mrtan <freemrtan@gmail.com>
2018-02-01 19:32:29 +0800

    集成样式

--- src/App.js
+++ src/App.js
@@ -21,42 +21,50 @@ const list = [
   }
 ]
 
-function Search(props){
-  const {value, onChange, children} = props
-  return(
-    <form>
-      {children}<input
-        type='text'
-        value={value}
-        onChange={onChange}
-        />
-    </form>
-  )  
+const largeColumn = {
+  width: '40%',
 }
 
-function Table(props){
-  const {list, pattern, onDismiss} = props;
-
-  return(
-    <div>
-      {list.filter(isSearched(pattern)).map(item =>
-        <div key={item.objectID}>
-          <span><a href={item.url}>{item.title}</a></span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>
-            <button
-              type='button'
-              onClick={() => onDismiss(item.objectID)}
-              >
-            Dismiss
-            </button>
-          </span>
-        </div>
-      )}
-    </div>
-  )
+const midColumn = {
+  width: '30%',
 }
+
+const smallColumn = {
+  width: '10%',
+}
+
+const Search = ({value, onChange, children}) =>
+  <form>
+    {children}<input
+      type='text'
+      value={value}
+      onChange={onChange}
+      />
+  </form>
+
+const Table = ({list, pattern, onDismiss}) =>
+  <div className='table'>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className='table-row'>
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>
+          {item.point}
+        </span>
+        <span style={smallColumn}>
+          <button
+            onClick={() => onDismiss(item.objectID)}
+            className='button-inline'
+            >
+          Dismiss
+          </button>
+        </span>
+      </div>
+    )}
+  </div>
 
 class App extends Component {
   constructor(props){
@@ -80,12 +88,14 @@ class App extends Component {
   render(){
     const {list,searchTerm} = this.state
     return(
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}>
-          Search
-        </Search>
+      <div className="page">
+        <div className='interactions'>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
         <Table
           list={list}
           pattern={searchTerm}
