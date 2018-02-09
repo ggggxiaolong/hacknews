982fb94eb335d0f38d3aac64b4d3807be11eafc4
mrtan <freemrtan@gmail.com>
2018-02-09 10:52:48 +0800

    添加jest测试代码

---
+++ src/components/Search/test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { Search } from "./index";
+
+describe('Search', () => {
+    it('renders withot crashing', () => {
+        const div = document.createElement('div')
+        ReactDOM.render(<Search>Search</Search>, div)
+    });
+    test('has a valid snapshot', () => {
+        const component = renderer.create(
+            <Search>Search</Search>
+        )
+        let tree = component.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
---
+++ src/components/Table/test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import {Table} from './index'
+
+describe('Table', () => {
+    const list = [
+        {title: '1', author: '1', num_comments: 1, points: 1, objectID: 'y'},
+        {title: '2', author: '2', num_comments: 2, points: 2, objectID: 'z'},
+    ]
+    const props = {list}
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+        ReactDOM.render(<Table {...props}/>, div)
+    })
+    test("has a valid snapshot", () => {
+        const component = renderer.create(
+            <Table {...props}/>
+        )
+        let tree = component.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
